9828a3d2af010cccf5e257fdef6f9d0741c73deb
Alberto <ajsaenz1091@gmail.com>
2021-07-15 14:50:06 -0400

    commit tree 70b4d7d54459b90a67b0bf257f99fba5f4077d7a

--- back-end/app/application.rb
+++ back-end/app/application.rb
@@ -8,14 +8,12 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/test/) 
-
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "response success"}.to_json ]]
-
+    elsif req.path.match(/tasks/)
+      return [200, {'Content-Type' => 'application/json'}, [{data: Task.all}.to_json]]
     else
       resp.write "Path Not Found"
-
     end
-
     resp.finish
   end
 
